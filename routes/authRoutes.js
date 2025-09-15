455144528ba7bbba33a1b41c2394b1d490d38432
Om005 <chavdaom84@gmail.com>
2025-09-16 00:10:24 +0530

    half way done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,15 @@
 import express from "express" 
-import {register, verifyAccount} from "../controllers/Auth.js";
-
+import {isAuthenticated, Logout, register, resetPassword, sendResetToken, Signin, verifyAccount, verifyResetToken} from "../controllers/Auth.js";
+import userAuth from "../middlewares/userAuth.js"
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.post('/account-verify', verifyAccount);
+authRouter.post('/singin', Signin);
+authRouter.post('/logout', Logout);
+authRouter.post('/is-auth', userAuth, isAuthenticated);
+authRouter.post('/send-reset-token', sendResetToken);
+authRouter.post('/verify-reset-token', verifyResetToken);
+authRouter.post('/reset-password', resetPassword);
+
 export default authRouter;
