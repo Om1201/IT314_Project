8145fb6f823cef3068e3d75be5c5cc67dad74894
Aryan Rathod <aryanrathod1515@gmail.com>
2025-09-16 12:10:55 +0530

    Added routes

--- models/UserModel.js
+++ models/UserModel.js
@@ -12,8 +12,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
-        min: 5
+        default: ''
     },
     verifyToken: {
         type: String,
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,9 @@
 import express from "express" 
 import {isAuthenticated, Logout, register, resetPassword, sendResetToken, Signin, verifyAccount, verifyResetToken} from "../controllers/Auth.js";
 import userAuth from "../middlewares/userAuth.js"
+import {login as googleLogin} from "../controllers/oAuthController.js";
+import {verifyToken as googleVerifyToken} from "../controllers/oAuthController.js";
+
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
@@ -11,5 +14,7 @@ authRouter.post('/is-auth', userAuth, isAuthenticated);
 authRouter.post('/send-reset-token', sendResetToken);
 authRouter.post('/verify-reset-token', verifyResetToken);
 authRouter.post('/reset-password', resetPassword);
+authRouter.post('/oauth/google/login', googleLogin);
+authRouter.post('/oauth/google/callback', googleVerifyToken);
 
 export default authRouter;
