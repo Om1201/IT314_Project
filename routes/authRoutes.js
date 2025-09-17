4ee7d5c11eb6531847bd930ade96be23a92a6ce9
Harsh-Chanchlani <202301152@dau.ac.in>
2025-09-18 01:04:29 +0530

    Some Controllers Added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,16 @@
 import express from "express" 
-import { Logout, register, Signin, verifyAccount } from "../controllers/Auth.js";
+import {isAuthenticated, Logout, register, resetPassword, sendResetToken, Signin, verifyAccount, verifyResetToken} from "../controllers/Auth.js";
+import userAuth from "../middlewares/userAuth.js"
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.post('/account-verify', verifyAccount);
 authRouter.post('/singin', Signin);
 authRouter.post('/logout', Logout);
+authRouter.post('/is-auth', userAuth, isAuthenticated);
+authRouter.post('/send-reset-token', sendResetToken);
+authRouter.post('/verify-reset-token', verifyResetToken);
+authRouter.post('/reset-password', resetPassword);
+
 
 export default authRouter;
